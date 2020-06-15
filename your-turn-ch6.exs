7debcca46653932b451a29521822bd387d304992
Jeff Sutherland <jeffdsuth@gmail.com>
2020-06-15 09:12:25 -0700

    Add binary search ex pg 62

--- your-turn-ch6.exs
+++ your-turn-ch6.exs
@@ -19,3 +19,16 @@ defmodule Recursion do
   def gcd(x, 0) when x >= 0, do: x
   def gcd(x, y) when x >= 0 and y >= 0, do: gcd(y, rem(x, y))
 end
+
+defmodule Chop do
+  def guess(actual, range) do
+    lo..hi = range
+    mid = div(lo + hi, 2)
+    IO.puts "Is it #{mid}"
+    next(actual, mid, lo, hi)
+  end
+
+  def next(actual, mid, _, _) when actual == mid, do: actual
+  def next(actual, mid, lo, _) when actual < mid, do: guess(actual, lo..mid)
+  def next(actual, mid, _, hi) when actual > mid, do: guess(actual, mid..hi)
+end
